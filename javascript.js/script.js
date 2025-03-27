f094521fd2d348ba119f50b1823f437b25d15099
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-27 06:52:28 +0100

    [UPD] EVENTO DA GENERARE

--- javascript.js/script.js
+++ javascript.js/script.js
@@ -1,4 +1,25 @@
-const genera = document.getElementById('generaBiglietto');
-const km = document.getElementById('kmDaPercorrere');
-const anni = document.getElementById('anniUtente');
+let genera = document.getElementById('generaBiglietto');
+let km = document.getElementById('kmDaPercorrere');
+let anni = document.getElementById('anniUtente');
 
+genera.addEventListener('click', function(event){
+    event.preventDefault();
+    km = document.getElementById('kmDaPercorrere').value.trim();
+    anni = document.getElementById('anniUtente').value.trim();
+
+    let price = numeroKm * 0.21;
+    let message;
+    if(anni < 18 ){ 
+                   price = price - price * 0.2;
+                   // SCRIVO IL MESSAGGIO DA MOSTRARE A VIDEO E LO ASSEGNO ALLA VARIABILE MESSAGE
+                   message = `Hai diritto ad uno sconto del 20%. Il prezzo del tuo biglietto è di ${price.toFixed(2)} €`;
+                    }
+    else if (anni > 65){
+                   price = price - price * 0.4;
+                   message = `Hai diritto ad uno sconto del 40%. Il prezzo del tuo biglietto è di ${price.toFixed(2)} €`;
+                    }
+    else 
+    {
+      message = `Il prezzo del tuo biglietto è ${price.toFixed(2)} €`;
+    }
+});
