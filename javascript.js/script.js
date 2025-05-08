e9471bb27f4fc521159d6a30ac1faee10f4978be
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-08 07:40:05 +0200

    [UPD] CORREZIONE DEL PROFESSORE

--- javascript.js/script.js
+++ javascript.js/script.js
@@ -1,4 +1,4 @@
-// RECUPERO ELEMENTI DEL DOM E/O DICHIARAZIONE ED INIZIALIZZAZIONE DELLE VARIABILI
+// RECUPERO ELEMENTI DEL DOM E/O DICHIARO ED INIZIALIZZO DELLE VARIABILI
 const km = document.getElementById('km');
 const age = document.getElementById('age');
 const button = document.getElementById('calculate');
@@ -7,31 +7,42 @@ const kmResult = document.getElementById('km-result');
 const priceResult = document.getElementById('price')
 
 // CORPO DEL PROGRAMMA 
-button.addEventListener('click', function(e){
+button.addEventListener('click', function (e) {
   e.preventDefault();
-  // valori recuperati dalla form
+
+  // VALORI RECUPERATI DALLA FORM 
   let kmValue = parseInt(km.value);
   let ageValue = parseInt(age.value);
-  // definizione della costante moltiplicativa determinante del prezzo
+
+  // DEFINISCO LA COSTANTE MOLTIPLICATIVA DETERMINANTE DEL PREZZO 
   const kmUnit = 0.21;
-  // dichiaro la variabile contenente il prezzo non inizializzata 
+
+  // DICHIARO LA VARIABILE CONTENENTE IL PREZZO NON INIZIALIZZATA
   let price;
-  // definisco i prezzi in base all'età
-  if(ageValue < 18){
+
+  // DEFINISCO I PREZZI IN BASE ALL'ETA'
+  if (ageValue < 18) {
     price = kmUnit * kmValue * 0.8;
   }
-  else if(ageValue > 65){
+  else if (ageValue > 65) {
     price = kmUnit * kmValue * 0.6;
   }
   else {
-  price = kmUnit * kmValue;
+    price = kmUnit * kmValue;
   }
+
   // console.log(price);
-  // mostro i risultati in pagina
-  ageResult.innerText = ageValue;
-  kmResult.innerText = kmValue;
 
-  priceResult.innerHTML = `<strong>${price.toFixed(2)}</strong>`;
-  
+  // MOSTRO I RISULTATI IN PAGINA
+  ageResult.innerHTML = `<strong>Kilometri : <br></strong>${kmValue}`;
+  kmResult.innerHTML = `<strong>Età : <br></strong>${ageValue}`;
 
+  priceResult.innerHTML = `<strong>Prezzo : <br>${price.toFixed(2)}€</strong>`;
+
+  const h2 = document.getElementById('summary');
+  h2.classList.remove('d-none');
+  ageResult.classList.remove('d-none');
+  kmResult.classList.remove('d-none');
+  priceResult.classList.remove('d-none');
 });
+
